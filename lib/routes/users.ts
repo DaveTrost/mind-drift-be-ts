36bc4971d0eae04340640680f3a5cb50af9f7524
Dave Trost <davetrost@gmail.com>
2020-01-17 17:09:42 -0800

    user aggregation for average session time

--- lib/routes/users.ts
+++ lib/routes/users.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { User } from '../models/User';
+import { Session } from '../models/Session';
 
 export default Router()
   .get('/users', ({ query }: Request, res: Response, next: NextFunction) => {
@@ -7,4 +8,11 @@ export default Router()
       .find({ userId: query.userId })
       .then(user => res.send(user))
       .catch(next);
+  })
+  .get('/users/averageTime', ({ query }: Request, res: Response, next: NextFunction) => {
+    const { userId } = query;
+    Session.schema.statics.averageSessionTime(userId)
+      .then((avg: number) => res.json(avg))
+      .catch(next);
   });
+  
